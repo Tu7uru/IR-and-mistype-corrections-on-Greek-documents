99c064a4f3a807a85e6f05ae8d8039cf5e09e6ba
Haralampos Varsamis <haralab0ss@gmail.com>
2022-03-31 21:24:43 +0300

    code to call localhost with certificates

---
+++ ps/ElasticSearch.ps1
@@ -0,0 +1,26 @@
+﻿param(
+    [switch]$loadBulk
+)
+
+if($loadBulk) {
+
+    $username = "elastic"
+    $securePwd = ConvertTo-SecureString "s+bomWnaC=oMdfXvAeSr" -AsPlainText -Force
+    $credential = New-Object System.Management.Automation.PSCredential ($username, $securePwd)
+
+    add-type @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+        public class TrustAllCertsPolicy : ICertificatePolicy {
+            public bool CheckValidationResult(
+             ServicePoint srvPoint, X509Certificate certificate,
+             WebRequest request, int certificateProblem) {
+                return true;
+            }
+        }
+"@
+
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+    #Invoke-RestMethod "https://localhost:9200/dataset/_bulk?pretty" -Method Post -ContentType 'application/x-ndjson' -InFile "..\dataset\dataset.json"
+    Invoke-RestMethod "https://localhost:9200" -Method GET -Credential $credential
+}
