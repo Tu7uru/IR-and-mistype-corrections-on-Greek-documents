f4a43863743ee9aa6cb1dabbdc8f8d26f76febe1
Haralampos Varsamis <haralab0ss@gmail.com>
2022-04-03 18:02:23 +0300

    Added script execution to convert json dataset Documents to required format of elastic search bulk insert

--- ps/ElasticSearch.ps1
+++ ps/ElasticSearch.ps1
@@ -1,5 +1,14 @@
 ﻿param(
-    [switch]$loadBulk
+    [Parameter(Mandatory = $true,ParameterSetName = 'Load')]
+    [switch]$loadBulk,
+    [Parameter(Mandatory = $true,ParameterSetName = 'Convert')]
+    [switch]$convertJsonDocsToBulkInsert,
+#    [Parameter(Mandatory = $true,ParameterSetName = 'Convert')]
+    [string]$index,
+#    [Parameter(Mandatory = $true,ParameterSetName = 'Convert')]
+    [string]$inputFilename,
+    [Parameter(Mandatory = $true,ParameterSetName = 'Convert')]
+    [string]$outputFilename
 )
 
 if($loadBulk) {
@@ -20,7 +29,48 @@ if($loadBulk) {
         }
 "@
 
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-    #Invoke-RestMethod "https://localhost:9200/dataset/_bulk?pretty" -Method Post -ContentType 'application/x-ndjson' -InFile "..\dataset\dataset.json"
-    Invoke-RestMethod "https://localhost:9200" -Method GET -Credential $credential
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+    Invoke-RestMethod "https://localhost:9200/$index/_bulk?pretty" -Method Post -ContentType 'application/x-ndjson' -InFile "..\$index\$inputFilename" -Credential $credential
+    #Invoke-RestMethod "https://localhost:9200" -Method GET -Credential $credential
+}
+elseif($convertJsonDocsToBulkInsert){
+    
+    #$inputFilename="dataset.json"
+    #$index = "dataset"
+    $jsonDataset = Get-Content -Path "..\dataset\$inputFilename" -Encoding UTF8 | ConvertFrom-Json
+    $ConvertedData = ""
+    foreach($doc in $jsonDataset.Documents) {
+        ## Create index json entry
+        if($null -ne $index -and $index.Trim() -ne "") {
+            $jsonIndexEntry = '{ "index" : { "_index" : "'+$index+'", "_id" : "'+$doc.ID+'" } }'+"`n"
+        }
+
+        ## Create data json entry
+
+        $firstField = $true
+        $jsonDataEntry = "{"
+        foreach($fieldProperty in $doc.PSObject.Properties) {
+            
+            $key = $fieldProperty.Name
+            if($fieldProperty.TypeNameOfValue -eq "System.String"){
+                $fieldProperty.Value = $fieldProperty.Value -replace '"','\"'
+                $value = "`""+$fieldProperty.Value+"`""
+            }
+            else {
+                $value = $fieldProperty.Value
+            }
+            
+            if($firstField) {
+                $jsonDataEntry += "`"$key`":$value"
+                $firstField = $false
+            }
+            else {
+                $jsonDataEntry += ",`"$key`":$value"
+            }
+        }
+        $jsonDataEntry += "}`n"
+        $ConvertedData += $jsonIndexEntry
+        $ConvertedData += $jsonDataEntry
+    }
+    $ConvertedData  | Out-File "..\dataset\$outputFilename.json" -Encoding utf8 -Force
 }
