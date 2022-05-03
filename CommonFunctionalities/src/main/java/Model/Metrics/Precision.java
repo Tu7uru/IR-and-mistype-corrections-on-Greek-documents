15e6f735dcb0386f8d86dc3a81dcfab587103374
Haralampos Varsamis <haralab0ss@gmail.com>
2022-05-03 17:42:21 +0300

    Restructured project

---
+++ CommonFunctionalities/src/main/java/Model/Metrics/Precision.java
@@ -0,0 +1,37 @@
+package Model.Metrics;
+
+import Model.Document;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Precision {
+
+    private double score;
+
+    public Precision() {
+        this.score = 0;
+    }
+
+    public double calculatePrecision(List<Document> docsRetrieved, HashSet<Integer> relevantDocIds ) {
+        int numRetrieved = docsRetrieved.size();
+        int numRelevantRetrieved = 0;
+
+        for(Document doc : docsRetrieved) {
+            if(relevantDocIds.contains(doc.getID())) {
+                numRelevantRetrieved++;
+            }
+        }
+        return ((double) numRelevantRetrieved) / numRetrieved;
+    }
+
+    public void setScore(double value) {
+        this.score = value;
+    }
+
+    public double getScore() {
+        return this.score;
+    }
+
+
+}
