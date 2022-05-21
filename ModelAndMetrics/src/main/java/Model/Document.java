6dc838dbc09ef1c99db88655f6cc965ab14df2e8
Haralampos Varsamis <haralab0ss@gmail.com>
2022-05-21 15:02:42 +0300

    Added objectID which is mandatory for Algolia

--- ModelAndMetrics/src/main/java/Model/Document.java
+++ ModelAndMetrics/src/main/java/Model/Document.java
@@ -2,27 +2,35 @@ package Model;
 
 public class Document {
     private int ID;
+    private String objectID; // Algolia required field
     private String Title;
     private String Body;
 
     public Document() {
         this.ID = -1;
+        this.objectID = "";
         this.Title = "";
         this.Body = "";
     }
 
     public Document(int id, String title, String body) {
         this.ID = id;
+        this.objectID = Integer.toString(id);
         this.Title = title;
         this.Body = body;
     }
 
     public void setID(int id) {
         this.ID = id;
+        this.objectID = Integer.toString(id);
     }
 
     public int getID() {
         return this.ID;
+    }
+
+    public String getObjectID() {
+        return this.objectID;
     }
 
     public void setTitle(String title) {
