76e7b860573ac71621ab707cb7096445f5990c4f
Haralampos Varsamis <haralab0ss@gmail.com>
2022-06-02 21:02:57 +0300

    Converted functionality to a more abstract flow and added extra functionalities(Metrics calculation,index creation,formatting)

--- ps/ElasticSearch.ps1
+++ ps/ElasticSearch.ps1
@@ -1,20 +1,46 @@
 ﻿param(
     [Parameter(Mandatory = $true,ParameterSetName = 'Load')]
     [switch]$loadBulk,
+    
+    [Parameter(Mandatory = $true,ParameterSetName = 'CreateIndex')]
+    [Parameter(Mandatory = $true,ParameterSetName = 'CreateIndexManually')]
+    [switch]$createIndex,
+    
+    [Parameter(Mandatory = $false,ParameterSetName = 'CreateIndex')]
+    [switch]$RemoveStopwords,
+    
+    [Parameter(Mandatory = $false,ParameterSetName = 'CreateIndex')]
+    [switch]$ApplyStemming,
+    
+    [Parameter(Mandatory = $false,ParameterSetName = 'CreateIndex')]
+    [String]$AnalyzerName,
+
+    [Parameter(Mandatory = $false,ParameterSetName = 'CreateIndexManually')]
+    [String]$IndexBodyDefinition,
+    
+    [Parameter(Mandatory = $true,ParameterSetName = 'Get')]
+    [switch]$retrieveDataset,
+    
     [Parameter(Mandatory = $true,ParameterSetName = 'Convert')]
     [switch]$convertJsonDocsToBulkInsert,
-#    [Parameter(Mandatory = $true,ParameterSetName = 'Convert')]
-    [string]$index,
-#    [Parameter(Mandatory = $true,ParameterSetName = 'Convert')]
+    
+    [Parameter(Mandatory = $true)]
+    [string]$indexName,
+    
+    [Parameter(Mandatory = $true,ParameterSetName = 'Convert')]
+    [Parameter(Mandatory = $true,ParameterSetName = 'Load')]
     [string]$inputFilename,
+    
     [Parameter(Mandatory = $true,ParameterSetName = 'Convert')]
     [string]$outputFilename
 )
 
-if($loadBulk) {
+Import-Module "$PSScriptRoot\ElasticSearch\Operations.psm1" -Force
+
+if($loadBulk -or $retrieveDataset -or $createIndex) {
 
     $username = "elastic"
-    $securePwd = ConvertTo-SecureString "s+bomWnaC=oMdfXvAeSr" -AsPlainText -Force
+    $securePwd = ConvertTo-SecureString "bczk1THwmVIciEZYeCaq" -AsPlainText -Force
     $credential = New-Object System.Management.Automation.PSCredential ($username, $securePwd)
 
     add-type @"
@@ -28,25 +54,84 @@ if($loadBulk) {
             }
         }
 "@
-
+    
     [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-    Invoke-RestMethod "https://localhost:9200/$index/_bulk?pretty" -Method Post -ContentType 'application/x-ndjson' -InFile "..\$index\$inputFilename" -Credential $credential
-    #Invoke-RestMethod "https://localhost:9200" -Method GET -Credential $credential
+
+    $headers = @{
+            "Content-Type"="application/json; charset=utf-8";
+            "OData-MaxVersion"="4.0";
+            "OData-Version"="4.0";
+    };
+
+
+    if($loadBulk) {
+        $body = Get-Content -Path "..\dataset\$inputFilename"
+        Invoke-RestMethod "https://localhost:9200/$indexName/_bulk?pretty" -Method Post -ContentType 'application/x-ndjson' -InFile "..\dataset\$inputFilename" -Credential $credential
+        #Invoke-RestMethod "https://localhost:9200/$indexName/_bulk?pretty" -Method Post -ContentType 'application/x-ndjson' -Body $body  -Credential $credential
+    }
+    elseif($createIndex) {
+#            $body = @"
+#{
+#"mappings": {
+#      "properties": {
+#          "ID": {"type":"integer"},
+#          "Title" :{"type":"text","analyzer": "my_greek_analyzer"},
+#          "Body": {"type":"text","analyzer": "my_greek_analyzer"}
+#    }
+#  },
+#  "settings": {
+#    "analysis": {
+#      "analyzer": {
+#        "my_greek_analyzer": {
+#          "type": "standard",
+#          "max_token_length": 150,
+#          "stopwords": "_greek_"
+#        }
+#      }
+#    }
+#  }
+#}
+#"@
+        $mappings = @"
+  "mappings": {
+      "properties": {
+          "ID": {"type":"integer"},
+          "Title" :{"type":"text","analyzer": "$AnalyzerName"},
+          "Body": {"type":"text","analyzer": "$AnalyzerName"}
+    }
+  }
+"@
+        if($ApplyStemming -and $RemoveStopwords) {
+            Set-Index -IndexName $indexName -Mappings $mappings -RemoveStopwords -ApplyStemming -AnalyzerName $AnalyzerName -Headers $headers -Credential $credential
+        }
+        elseif($ApplyStemming) {
+            Set-Index -IndexName $indexName -Mappings $mappings  -ApplyStemming -AnalyzerName $AnalyzerName -Headers $headers -Credential $credential
+        }
+        elseif($RemoveStopwords) {
+            Set-Index -IndexName $indexName -Mappings $mappings -RemoveStopwords  -AnalyzerName $AnalyzerName -Headers $headers -Credential $credential
+        }
+        else {
+            Set-Index -IndexName $indexName -IndexDefinition $IndexBodyDefinition -Headers $headers -Credential $credential
+        }
+    }
+    else {
+         $tst = Invoke-RestMethod "https://localhost:9200/dataset/_search?q= Αφρική" -Headers $headers -ContentType 'text/plain; charset=utf-8' -Method GET  -Credential $credential 
+         Search -Credentials $credential
+    }
 }
 elseif($convertJsonDocsToBulkInsert){
     
     #$inputFilename="dataset.json"
     #$index = "dataset"
     $jsonDataset = Get-Content -Path "..\dataset\$inputFilename" -Encoding UTF8 | ConvertFrom-Json
-    $ConvertedData = ""
+    $convertedData = ""
     foreach($doc in $jsonDataset.Documents) {
         ## Create index json entry
-        if($null -ne $index -and $index.Trim() -ne "") {
-            $jsonIndexEntry = '{ "index" : { "_index" : "'+$index+'", "_id" : "'+$doc.ID+'" } }'+"`n"
+        if($null -ne $indexName -and $indexName.Trim() -ne "") {
+            $jsonIndexEntry = '{ "index" : { "_index" : "'+$indexName+'", "_id" : "'+$doc.ID+'" } }'+"`n"
         }
 
         ## Create data json entry
-
         $firstField = $true
         $jsonDataEntry = "{"
         foreach($fieldProperty in $doc.PSObject.Properties) {
@@ -69,8 +154,22 @@ elseif($convertJsonDocsToBulkInsert){
             }
         }
         $jsonDataEntry += "}`n"
-        $ConvertedData += $jsonIndexEntry
-        $ConvertedData += $jsonDataEntry
+        $convertedData += $jsonIndexEntry
+        $convertedData += $jsonDataEntry
     }
-    $ConvertedData  | Out-File "..\dataset\$outputFilename.json" -Encoding utf8 -Force
+    $convertedData  | Out-File "..\dataset\$outputFilename.json" -Encoding utf8 -Force
 }
+#$defaultEncoding = [System.Text.Encoding]::GetEncoding('ISO-8859-1')
+#
+#$string = 'Paulé'
+#
+#$utf8Bytes = [System.Text.Encoding]::UTf8.GetBytes($x)
+#
+#$decoded = $defaultEncoding.GetString($utf8bytes)
+#
+#$object = New-Object psobject -Property @{
+#    Original = $x
+#    Decoded  = $decoded
+#}
+#
+#$object | Format-Table -AutoSize
---
+++ ps/ElasticSearch/Operations.psm1
@@ -0,0 +1,276 @@
+﻿function Set-Index {
+    param(
+        $IndexName,
+        [Parameter(Mandatory=$true,ParameterSetName = 'ManuallyGenerated')]
+        [String]$IndexDefinition,
+        $Headers,
+        [PSCredential] $Credential,
+        [Parameter(Mandatory=$false,ParameterSetName = 'SystemGenerated')]
+        [switch]$RemoveStopwords,
+        [Parameter(Mandatory=$false,ParameterSetName = 'SystemGenerated')]
+        [switch]$ApplyStemming,
+        [Parameter(Mandatory=$true,ParameterSetName = 'SystemGenerated')]
+        [String]$AnalyzerName,
+        [Parameter(Mandatory=$false,ParameterSetName = 'SystemGenerated')]
+        [String]$Mappings
+        
+    )
+    
+    if($RemoveStopwords -and $ApplyStemming) {
+        $settings = @"
+        "settings": {
+    "analysis": {
+      "filter": {
+        "greek_stop": {
+          "type":       "stop",
+          "stopwords":  "_greek_" 
+        },
+        "greek_lowercase": {
+          "type":       "lowercase",
+          "language":   "greek"
+        },
+        "greek_stemmer": {
+          "type":       "stemmer",
+          "language":   "greek"
+        }
+      },
+      "analyzer": {
+        "$AnalyzerName": {
+          "tokenizer":  "standard",
+          "filter": [
+            "greek_lowercase",
+            "greek_stop",
+            "greek_stemmer"
+          ]
+        }
+      }
+    }
+  }
+"@
+        if($null -ne $Mappings -and $Mappings.Length -gt 0) {
+            $body = @"
+{
+    $mappings,
+    $settings
+}           
+"@
+        }
+    }
+    elseif($RemoveStopwords) {
+        $settings = @"
+        "settings": {
+    "analysis": {
+      "filter": {
+        "greek_stop": {
+          "type":       "stop",
+          "stopwords":  "_greek_" 
+        },
+        "greek_lowercase": {
+          "type":       "lowercase",
+          "language":   "greek"
+        }
+      },
+      "analyzer": {
+        "rebuilt_greek": {
+          "tokenizer":  "standard",
+          "filter": [
+            "greek_lowercase",
+            "greek_stop"
+          ]
+        }
+      }
+    }
+  }
+"@
+        if($null -ne $Mappings -and $Mappings.Length -gt 0) {
+            $body = @"
+{
+    $mappings,
+    $settings
+}
+            
+"@
+        }
+    }
+    elseif($ApplyStemming) {
+        $settings = @"
+        "settings": {
+    "analysis": {
+      "filter": {
+        "greek_lowercase": {
+          "type":       "lowercase",
+          "language":   "greek"
+        },
+        "greek_stemmer": {
+          "type":       "stemmer",
+          "language":   "greek"
+        }
+      },
+      "analyzer": {
+        "rebuilt_greek": {
+          "tokenizer":  "standard",
+          "filter": [
+            "greek_lowercase",
+            "greek_stemmer"
+          ]
+        }
+      }
+    }
+  }
+"@
+        if($null -ne $Mappings -and $Mappings.Length -gt 0) {
+            $body = @"
+{
+    $mappings,
+    $settings
+}
+            
+"@
+        }
+    }
+    else {
+        $body = $IndexDefinition
+    }
+
+    if($body -ne $null -and $body.Length -gt 0) {
+        Invoke-RestMethod "https://localhost:9200/$($IndexName.ToLower())" -Headers $Headers -Method PUT -Body $body -Credential $Credential
+    }
+}
+
+function Format-RelevantDocs {
+    param(
+        $RelevantDocs
+    )
+    
+    $UpdatedRelavantDocs = @()
+
+    foreach($Document in $relevantDocs) {
+        $tokenizedDocument = $Document | ConvertFrom-String
+        $formattedDocument = [PSCustomObject]@{
+            DocID = [int]$tokenizedDocument.P1.Split("=")[1].replace(";","")
+            RelevanceRank = [int]$tokenizedDocument.P2.Split("=")[1].replace("}","")
+        }
+        $UpdatedRelavantDocs += $formattedDocument
+    }
+
+    return $UpdatedRelavantDocs
+}
+
+function Get-Data {
+    param(
+        [String]$path
+    )
+
+    $data = Get-Content -Path $path -Encoding UTF8 | ConvertFrom-Json
+    return $data
+}
+
+function Get-Precision {
+    param(
+        $DocumentsRetrieved,
+        $DocumentsExpected
+    )
+    if($null -eq $DocumentsRetrieved -or $null -eq $DocumentsExpected) {
+        return 0;
+    }
+
+    $relevantRetrieved = 0
+    $retrieved = 0
+    foreach($documentRetrieved in $DocumentsRetrieved) {
+        if($DocumentsExpected | Where-Object {$_.DocID -eq $documentRetrieved}) {
+            $relevantRetrieved++
+        }
+        $retrieved++
+    }
+
+    return $relevantRetrieved / $retrieved
+}
+
+function Get-Recall {
+    param(
+        $DocumentsRetrieved,
+        $DocumentsExpected
+    )
+    if($null -eq $DocumentsRetrieved -or $null -eq $DocumentsExpected) {
+        return 0;
+    }
+
+    $relevantRetrieved = 0
+    $retrieved = 0
+    $relevant = 0
+    foreach($documentRetrieved in $DocumentsRetrieved) {
+        if($DocumentsExpected | Where-Object {$_.DocID -eq $documentRetrieved}) {
+            $relevantRetrieved++
+        }
+        $retrieved++
+    }
+
+    foreach($relevantDocument in $DocumentsExpected) {
+        $relevant++
+    }
+
+    return $relevantRetrieved / $relevant
+}
+
+function Get-F_Measure {
+    param(
+        $Precision,
+        $Recall
+    )
+    if($Precision + $Recall -eq 0) {
+        return 0;
+    }
+    return (2* $Precision * $Recall) / ($Precision + $Recall)
+}
+
+function Get-Fall_Out {
+    param(
+        $DocumentsRetrieved,
+        $DocumentsExpected,
+        $AllDocuments
+    )
+    if($null -eq $DocumentsRetrieved -or $null -eq $DocumentsExpected) {
+        return 0;
+    }
+
+    $relevant = 0
+    $nonRelevantRetrieved = 0
+    foreach($documentRetrieved in $DocumentsRetrieved) {
+        if($null -eq ($DocumentsExpected | Where-Object {$_.DocID -eq $documentRetrieved})) {
+            $nonRelevantRetrieved++
+        }
+    }
+
+    foreach($relevantDocument in $DocumentsExpected) {
+        $relevant++
+    }
+    $nonRelevant = $AllDocuments.Count - $relevant
+    return $nonRelevantRetrieved / $nonRelevant
+}
+
+function Search {
+    
+    param(
+        [PSCredential] $Credentials,
+        $IndexName
+    )
+
+
+    $data = Get-Data -path "../dataset/dataset.json"
+    $documents = $data.Documents
+    $queries = $data.Queries
+    
+    foreach($query in $queries) {
+        $searchResult = Invoke-RestMethod "https://localhost:9200/$IndexName/_search?q=$($query.Query)" -Headers $headers -ContentType 'text/plain; charset=utf-8' -Method GET -Credential $Credentials
+        $idsRetrieved = $searchResult.hits.hits._source.ID
+        $idsExpected = Format-RelevantDocs -RelevantDocs $query.RelevantDocs
+        
+        $precision = Get-Precision -DocumentsRetrieved $idsRetrieved -DocumentsExpected $idsExpected
+        $recall = Get-Recall -DocumentsRetrieved $idsRetrieved -DocumentsExpected $idsExpected
+        Get-F_Measure -Precision $precision -Recall $recall
+        Get-Fall_Out -DocumentsRetrieved $idsRetrieved -DocumentsExpected $idsExpected -AllDocuments $documents
+    }
+}
+
+Export-ModuleMember -Function Search
+Export-ModuleMember -Function Set-Index
