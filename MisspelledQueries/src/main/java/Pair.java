61e5b62dd62f030cbe726f8e0683004f73105851
Haralampos Varsamis <haralab0ss@gmail.com>
2022-07-20 15:48:17 +0300

    Added class to hold pair objects

---
+++ MisspelledQueries/src/main/java/Pair.java
@@ -0,0 +1,9 @@
+public class Pair<L, R> {
+        public final L left;
+        public final R right;
+
+        public Pair(L t, R u) {
+            this.left= t;
+            this.right= u;
+        }
+    }
