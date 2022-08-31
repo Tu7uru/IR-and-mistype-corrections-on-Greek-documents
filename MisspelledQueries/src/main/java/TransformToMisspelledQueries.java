fdd914c4dca39aa12e4c9b90b3bfbfc88cf32582
Haralampos Varsamis <haralab0ss@gmail.com>
2022-08-31 12:08:04 +0300

    Updated letters to be static

--- MisspelledQueries/src/main/java/TransformToMisspelledQueries.java
+++ MisspelledQueries/src/main/java/TransformToMisspelledQueries.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class TransformToMisspelledQueries {
 
-    public Character[] greekLetters = {'α','β','γ','δ','ε','ζ','η','θ','ι','κ','λ','μ',
+    public static Character[] greekLetters = {'α','β','γ','δ','ε','ζ','η','θ','ι','κ','λ','μ',
             'ν','ξ','ο','π','ρ','σ','τ','υ','φ','χ','ψ','ω'};
 
     public static String addChar(String str, char ch, int position) {
@@ -14,7 +14,6 @@ public class TransformToMisspelledQueries {
         return str.substring(0, position) + ch + str.substring(position + 1);
     }
 
-
     public static String removeChar(String str, int position) {
         if(position > 0) {
             return str.substring(0, position) + str.substring(position + 1);
@@ -23,7 +22,7 @@ public class TransformToMisspelledQueries {
         }
     }
 
-    public ArrayList<String> AddExtraCharacter(ArrayList<String> tokenizedQueries) {
+    public static ArrayList<String> AddExtraCharacter(ArrayList<String> tokenizedQueries) {
         int length;
         int index;
         int numofletters = 24;
@@ -35,14 +34,14 @@ public class TransformToMisspelledQueries {
         for(String query : tokenizedQueries) {
             length = query.length();
             index = random.nextInt(length);
-            transformedQuery = addChar(query,this.greekLetters[random.nextInt(numofletters)],index);
+            transformedQuery = addChar(query,greekLetters[random.nextInt(numofletters)],index);
             transformedQueries.add(transformedQuery);
         }
 
         return transformedQueries;
     }
 
-    public ArrayList<String> RemoveCharacter(ArrayList<String> tokenizedQueries) {
+    public static ArrayList<String> RemoveCharacter(ArrayList<String> tokenizedQueries) {
         int length;
         int index;
         int numofletters = 24;
@@ -61,7 +60,7 @@ public class TransformToMisspelledQueries {
         return transformedQueries;
     }
 
-    public ArrayList<String> IncorrectCharacter(ArrayList<String> tokenizedQueries) {
+    public static ArrayList<String> IncorrectCharacter(ArrayList<String> tokenizedQueries) {
         int length;
         int index;
         int numofletters = 24;
@@ -73,15 +72,51 @@ public class TransformToMisspelledQueries {
         for(String query : tokenizedQueries) {
             length = query.length();
             index = random.nextInt(length);
-            transformedQuery = addChar(query,this.greekLetters[random.nextInt(numofletters)],index);
+            transformedQuery = replaceChar(query,greekLetters[random.nextInt(numofletters)],index);
             transformedQueries.add(transformedQuery);
         }
         return transformedQueries;
     }
+
+    public static ArrayList<String> IncorrectSurroundingCharacter(ArrayList<String> tokenizedQueries) {
+        int length;
+        int index;
+        int numofletters;
+        char letter;
+        ArrayList<Character> surroundingCharacters;
+        Random random = new Random();
+
+        String transformedQuery;
+        ArrayList<String> transformedQueries = new ArrayList<>();
+
+        for(String query : tokenizedQueries) {
+            length = query.length();
+            index = random.nextInt(length);
+            letter = query.charAt(index);
+            //System.out.println(letter);
+            surroundingCharacters = QueryCorrection.GetSurroundingCharacters(letter,1);
+            if(surroundingCharacters != null) {
+                numofletters = surroundingCharacters.size();
+                //System.out.println(numofletters);
+                transformedQuery = replaceChar(query, surroundingCharacters.get(random.nextInt(numofletters)), index);
+                transformedQueries.add(transformedQuery);
+            }
+        }
+        return transformedQueries;
+    }
+
     public static void main(String[] args) {
         String x = "κάρδαμο";
         System.out.println(addChar(x,'ζ' ,6));
         System.out.println(removeChar(x ,0));
         System.out.println(replaceChar(x,'ζ' ,0));
+
+        ArrayList<String> qs = new ArrayList<>();
+        qs.add("κάρδαμο");
+        System.out.println(AddExtraCharacter(qs));
+        System.out.println(RemoveCharacter(qs));
+        System.out.println(IncorrectCharacter(qs));
+        System.out.println(IncorrectSurroundingCharacter(qs));
+
     }
 }
